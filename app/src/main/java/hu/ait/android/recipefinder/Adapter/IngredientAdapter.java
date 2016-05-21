bc978bb6f24c7a01884bd0bfe8e68ccc43706005
Samantha Causey <causeys01@gmail.com>
2016-05-21 17:16:08 +0200

    RecipeAdapter added.

---
+++ app/src/main/java/hu/ait/android/recipefinder/Adapter/IngredientAdapter.java
@@ -0,0 +1,7 @@
+package hu.ait.android.recipefinder.Adapter;
+
+/**
+ * Created by samanthacausey on 5/20/16.
+ */
+public class IngredientAdapter {
+}
---
+++ app/src/main/java/hu/ait/android/recipefinder/Adapter/RecipeAdapter.java
@@ -0,0 +1,8 @@
+package hu.ait.android.recipefinder.Adapter;
+
+/**
+ * Created by samanthacausey on 5/20/16.
+ */
+public class RecipeAdapter {
+    /* Responsible for providing views that represent items in the Ingredients data set. */
+}
